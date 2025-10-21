636484c6d6d1050a73a2a7433503a06597de79e9
Diego Bruno Vogel <diegobvogel@gmail.com>
2025-10-21 15:06:13 -0300

    Exercício LeetCode 'Two Sum'

---
+++ TowSum.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String s = "ac";
+        System.out.println(sol.longestPalindrome(s)); // Output could be "bab" or "aba"
+    }
+
+    public String longestPalindrome(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            int posLetraRepetida = posicaoLetraRepetida(s, s.charAt(i));
+            if (posLetraRepetida != i) {
+                return s.substring(i, posLetraRepetida + 1);
+            }
+        }
+        return s;
+    }
+
+    private int posicaoLetraRepetida(String s, char c){
+        for(int i = s.length() - 1; i >= 0; i--){
+            if(s.charAt(i) == c){
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+}
